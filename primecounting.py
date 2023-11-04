8a323cc57265454b6880f18817b2140e2e4b9101
theblakeyerian <55857899+theblakeyerian@users.noreply.github.com>
2023-11-04 02:19:13 -0700

    Update primecounting.py

--- primecounting.py
+++ primecounting.py
@@ -1,9 +1,11 @@
-allcounters = [[1,1]]
-maxtime =1000
-for globaltime in range(2,maxtime):  
-    if not any([x[1] == 0 for x in allcounters]):
-        allcounters.append([globaltime,globaltime])         
-    allcounters = [[counter[0], counter[1] if counter[1] != 0 else counter[0]] for counter in allcounters] 
-    allcounters = [[x[0], x[1] - 1] for x in allcounters]  
-    allcounters[0][1] = 1 
-thePrimes = [element[0] for element in allcounters]
+#written by blake yerian, 8-1-2023
+def primecounting(maxtime):
+    allcounters = [[1,1]]
+    for globaltime in range(2,maxtime):  
+        if not any([x[1] == 0 for x in allcounters]):
+            allcounters.append([globaltime,globaltime])         
+        allcounters = [[counter[0], counter[1] if counter[1] != 0 else counter[0]] for counter in allcounters] 
+        allcounters = [[x[0], x[1] - 1] for x in allcounters]  
+        allcounters[0][1] = 1 
+    thePrimes = [element[0] for element in allcounters]
+    return thePrimes
