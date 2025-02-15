02f4498d0b828355d04429a06d34dc7a0bdfa355
theblakeyerian <55857899+theblakeyerian@users.noreply.github.com>
2025-02-14 23:36:05 -0800

    Update primecounting.py
    a little cleaner

--- primecounting.py
+++ primecounting.py
@@ -1,10 +1,10 @@
 #written by blake yerian, 8-1-2023
 def primecounting(maxtime):
-    allcounters = [[1,1]]
-    for globaltime in range(2,maxtime):  
-        if not any([x[1] == 0 for x in allcounters]):
-            allcounters.append([globaltime,globaltime])         
-        allcounters = [[counter[0], counter[1] if counter[1] != 0 else counter[0]] for counter in allcounters] 
-        allcounters = [[x[0], x[1] - 1] for x in allcounters]  
-        allcounters[0][1] = 1 
-    return [element[0] for element in allcounters]
+    counters = [[1, 1]]
+    for t in range(2, 50):
+        if not any(c[1] == 0 for c in counters):
+            counters.append([t, t])
+        counters = [[c[0], c[1] - 1 if c[1] != 0 else c[0]] for c in counters]
+        counters[0][1] = 1
+result = [c[0] for c in counters]
+
